0ae4d12550d4c94069c30e6f1433fb4c555c95a6
inthends <87696974@qq.com>
2023-06-19 21:05:25 +0800

    bug

--- src/pages/base/base.js
+++ src/pages/base/base.js
@@ -1,12 +1,22 @@
-import {Component} from 'react';
-import { Platform } from 'react-native';
+// import {Component} from 'react';
+// import { Platform } from 'react-native'; 
+// export default class BasePage extends Component {
+//     static headerForceInset = Platform.OS === 'ios' ? {top:20} : undefined
+//     constructor(props) {
+//         super(props); 
+//     } 
+// }
 
+import { Component } from "react";
+import { Platform } from "react-native";
+import ScreenUtil from "../../utils/screen-util";
 
 export default class BasePage extends Component {
-    static headerForceInset = Platform.OS === 'ios' ? {top:20} : undefined
-    constructor(props) {
-        super(props);
-
-    }
-
+  static headerForceInset =
+    Platform.OS === "ios" ? { top: ScreenUtil.navigationHeight() } : undefined;
+  // { top: ScreenUtil.isIphoneX() ? 44 : 20 } 
+  constructor(props) {
+    super(props);
+  }
 }
+
