5b178f58d8fea0275f5b3d0f32d3d4fc8262548a
zs <742155372@qq.com>
2023-07-06 10:39:07 +0800

    适配屏幕

--- src/pages/base/base.js
+++ src/pages/base/base.js
@@ -1,12 +1,13 @@
-import {Component} from 'react';
-import { Platform } from 'react-native';
-
+import { Component } from "react";
+import { Platform } from "react-native";
+import ScreenUtil from "../../utils/screen-util";
 
 export default class BasePage extends Component {
-    static headerForceInset = Platform.OS === 'ios' ? {top:20} : undefined
-    constructor(props) {
-        super(props);
+  static headerForceInset =
+    Platform.OS === "ios" ? { top: ScreenUtil.navigationHeight() } : undefined;
+  // { top: ScreenUtil.isIphoneX() ? 44 : 20 }
 
-    }
-
+  constructor(props) {
+    super(props);
+  }
 }
